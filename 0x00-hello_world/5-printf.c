0df505b4b594c1585ece0382da21cf038412d087
sepribo <sepribopepple@yahoo.com>
2023-02-16 18:25:59 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main - this is the main function
+ * main - this is the main functiom
  *
- * Return: 0 when successful
+ * Return: 0 when successfull
  */
+
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-        return: (0)
+	return (0);
 }
-
