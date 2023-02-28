0c02cf555c0bddea9ab42538c06a872d6ff31e36
sepribo <sepribopepple@yahoo.com>
2023-02-28 13:56:40 -0800

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
-#include "main.h'
+#include "main.h"
 
 /**
-* reset_to_98 - update the value of n to 98
-* @n: input
-* Return: n
-*/
+ * reset_to_98 - update the value of n to 98
+ * @n: input
+ * Return: n
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
